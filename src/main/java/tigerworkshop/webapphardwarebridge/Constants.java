a2f17f998f6a97d78e86ee33fe1a4228ac96fd1b
Tiger Fok <tiger@tiger-workshop.com>
2020-09-27 02:41:20 +0800

    Bump version

--- src/main/java/tigerworkshop/webapphardwarebridge/Constants.java
+++ src/main/java/tigerworkshop/webapphardwarebridge/Constants.java
@@ -3,5 +3,5 @@ package tigerworkshop.webapphardwarebridge;
 public class Constants {
     public static String APP_NAME = "WebApp Hardware Bridge";
     public static String APP_ID = "tigerworkshop.webapphardwarebridge";
-    public static String VERSION = "1.0.0-alpha1";
+    public static String VERSION = "1.0.0-alpha2";
 }
